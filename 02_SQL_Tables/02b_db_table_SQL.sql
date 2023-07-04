e5482bf884156e2162a33adf3a38d944e156e669
eva-comcave <e.moritz.comcave@gmail.com>
2023-07-04 10:59:38 +0000

    2tab

---
+++ 02_SQL_Tables/02b_db_table_SQL.sql
@@ -0,0 +1,39 @@
+-- Strukturen:
+
+/* Kommentar 1: Block */
+# Zeilenkommentar 1
+-- Zeilenkommentar 1
+
+
+# DB boo löschen, wenn vorhanden
+DROP DATABASE IF EXISTS boo;
+
+
+# neu anlegen
+CREATE DATABASE IF NOT EXISTS boo;
+
+SHOW DATABASES;
+
+# Tabelle anlegen:
+
+USE boo;
+
+CREATE TABLE test
+(
+ name VARCHAR(20),
+    age INT 
+    );
+    
+    # alle Tabellen in der DB anzeigen
+    SHOW TABLES;
+
+# Struktur der Tabelle zeigen
+DESCRIBE test;
+
+# Daten in Tabelle füllen
+INSERT INTO test(name,age) VALUES ("Grizabella",29);
+INSERT INTO test(age,name) VALUES (35,"Alonzo");
+INSERT INTO test VALUES ();
+
+# Inhalte der Tabelle anzeigen lassen
+SELECT * FROM test;
