84632db755187968ad0ae0821da6c885be3e29b2
eva-comcave <e.moritz.comcave@gmail.com>
2023-07-04 10:40:49 +0000

    tabelle1

--- 02_SQL_Tables/02a_db_table_SQL.sql
+++ 02_SQL_Tables/02a_db_table_SQL.sql
@@ -7,12 +7,36 @@
 # Datenbanken auf Server anzeigen:
 SHOW DATABASES;
 
-# DB boo anlegen, wenn noch nicht vorhanden
-CREATE DATABASE IF NOT EXISTS boo;
-
-SHOW DATABASES;
-
+# DB boo löschen, wenn vorhanden
 DROP DATABASE IF EXISTS boo;
 
 SHOW DATABASES;
 
+# neu anlegen
+CREATE DATABASE IF NOT EXISTS boo;
+
+SHOW DATABASES;
+
+# Tabelle anlegen:
+
+USE boo;
+
+CREATE TABLE test
+(
+ name VARCHAR(20),
+    age INT 
+    );
+    
+    # alle Tabellen in der DB anzeigen
+    SHOW TABLES;
+
+# Struktur der Tabelle zeigen
+DESCRIBE test;
+
+# Daten in Tabelle füllen
+INSERT INTO test(name,age) VALUES ("Grizabella",29);
+INSERT INTO test(age,name) VALUES (35,"Alonzo");
+INSERT INTO test VALUES ();
+
+# Inhalte der Tabelle anzeigen lassen
+SELECT * FROM test;
