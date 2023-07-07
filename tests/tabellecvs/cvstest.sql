2153c73012575f444f94106abeb037a42f217a42
eva-comcave <e.moritz.comcave@gmail.com>
2023-07-07 10:36:00 +0000

    where

--- tests/tabellecvs/cvstest.sql
+++ tests/tabellecvs/cvstest.sql
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS tabelle.patente
 (
     id INT NOT NULL PRIMARY KEY,        -- ID
     bundesland VARCHAR(50) NOT NULL,   -- Bundesland
-    2001 INT(5) NOT NULL,          -- Jahreszahl
+    2001 INT(5) NOT NULL          -- Jahreszahl
     
 );
 
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS tabelle.patente
 DESCRIBE tabelle.patente;
 
 -- CSV-Datei parsen
-LOAD DATA LOCAL INFILE "tests/tabelle cvs/patent.csv"
+LOAD DATA LOCAL INFILE "tests/tabellecvs/patent.csv"
 INTO TABLE tabelle.patente
 FIELDS TERMINATED BY ";" 
 LINES TERMINATED BY "\n" 
