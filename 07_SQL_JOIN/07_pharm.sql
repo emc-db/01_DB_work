304c636b2bd71c6b7a9804156de1287c73f9ef66
eva-comcave <e.moritz.comcave@gmail.com>
2023-07-14 08:08:46 +0000

    pharm

--- 07_SQL_JOIN/07_pharm.sql
+++ 07_SQL_JOIN/07_pharm.sql
@@ -12,7 +12,7 @@ INSERT INTO Hersteller VALUES
 (2, 'GSK'),
 (3, 'Merck');
 
-SELECT * FROM Hersteller;
+#SELECT * FROM Hersteller;
 
 CREATE TABLE Wirkstoff (
 WirkstoffID INTEGER PRIMARY KEY,
@@ -32,20 +32,40 @@ INSERT INTO Wirkstoff VALUES
 CREATE TABLE Arzneimittel (
 AMID INTEGER PRIMARY KEY,
 Bezeichnung VARCHAR  (300),
-HerstellerID
-Preis
-    rezeptpflichtig BOOLEAN
-);
-
+HerstellerID INTEGER,
+Preis VARCHAR (250),
+rezeptpflichtig BOOLEAN
+    );
+    
+    ALTER TABLE Arzneimittel
+    ADD CONSTRAINT Herst_FK FOREIGN KEY (HerstellerID) REFERENCES Hersteller(HerstellerID)
+    
+    
 INSERT INTO Arzneimittel VALUES
 
-(1, 'Aspirin', 1, 7.99,),
-(2, 'EDTA-Kapseln', 2, 4.99,),
-(3, 'Obnitix', 3, 19.99,)
-(4, 'Targin', 1, 14.00,)
+(1, 'Aspirin', 1, '7.99 €', false),
+(2, 'EDTA-Kapseln', 2, '4.99 €', false),
+(3, 'Obnitix', 3, '19.99 €', true),
+(4, 'Targin', 1, '14.99', true);
 
 
 CREATE TABLE Arzneimittelwirkstoffe(
+    AMID INTEGER NOT NULL,
+    WirkstoffID INTEGER NOT NULL,
+    Menge VARCHAR (250),
+    
+    PRIMARY KEY (AMID , WirkstoffID)
+);
 
+ALTER TABLE Arzneimittelwirkstoffe
+ADD CONSTRAINT AM_FK FOREIGN KEY (AMID) REFERENCES Arzneimittel(AMID),
+ADD CONSTRAINT Wirk_FK FOREIGN KEY (WirkstoffID) REFERENCES Wirkstoff(WirkstoffID);
 
-)
+INSERT INTO Arzneimittelwirkstoffe VALUES
+(1, 1, '100mg'),
+(2, 2, '10mg'),
+(3, 3, '2µM'),
+(4, 4, '1g'),
+(4, 5, '1:100');
+
+SELECT * FROM Arzneimittelwirkstoffe;
